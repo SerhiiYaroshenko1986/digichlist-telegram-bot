7212b6f458e7f55156afcbf054580e6041dcbb2c
Serhii Yaroshenko <datextdk@gmail.com>
2021-02-01 15:12:26 +0200

    add heroku config bot

--- bot.js
+++ bot.js
@@ -1,3 +1,9 @@
 const config = require("config");
 const Telegraf = require("telegraf");
 module.exports = bot = new Telegraf(config.get("token"));
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || "https://digihlist-bot.herokuapp.com";
+
+bot.telegram.setWebhook(`${URL}/bot${config.get("token")}`);
+bot.startWebhook(`/bot${config.get("token")}`, null, PORT);
