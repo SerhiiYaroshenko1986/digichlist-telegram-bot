00fa0a1059325f61c18fa01b3aacd3cc1d7c7e8d
Serhii Yaroshenko <datextdk@gmail.com>
2021-02-01 12:13:45 +0200

    change base url

--- services/render.js
+++ services/render.js
@@ -4,50 +4,45 @@ const Requests = require("./utils");
 const serviceRequest = new Requests();
 
 const bot = require("../bot");
-const { Markup, Extra } = require("telegraf");
+
 class Render {
   defect_id = "";
   payload = {};
   status = "";
-  getDefectsTemplate(ctx, defects, actionTriger) {
+  getDefectsTemplate(ctx, defects, actionTriger, feature, buttonTemplate) {
+    let comments = null;
     const details = defects.map((elem) => {
-      actionTriger.push(`${elem._id},fixing`);
-      actionTriger.push(`${elem._id},solved`);
-      if (elem.attachment_id !== "" && elem.attachment_id !== "id") {
-        const send = buttons.detailsBtn(elem._id);
-        send.caption = `Дефект під номером ${elem._id}\nКімната: ${elem.room}\nОпис пошкодження ${elem.title}`;
-        ctx.replyWithPhoto(elem.attachment_id, send);
-      } else {
-        ctx.replyWithHTML(
-          `Дефект під номером <b><i>${elem._id}</i></b>\n
-           Кімната: <b><i>${elem.room}</i></b>\n
-           Опис пошкодження <b><i>${elem.title}</i></b>`,
-          buttons.detailsBtn(elem._id)
-        );
+      actionTriger.push(`${elem._id},${feature[0]}`);
+      actionTriger.push(`${elem._id},${feature[1]}`);
+      if (elem.comments.length !== 0) {
+        comments = elem.comments.map((item) => item.message).join(", ");
       }
-    });
-    return details;
-  }
-  getFixingDefects(ctx, defects, actionTriger) {
-    const details = defects.map((elem) => {
-      actionTriger.push(elem._id);
-      if (elem.attachment_id !== "" && elem.attachment_id !== "id") {
-        const send = buttons.fixingBtn(elem._id);
-        send.caption = `Дефект під номером: ${elem._id}\nКімната: ${elem.room}\nОпис пошкодження ${elem.title}`;
+      if (elem.attachment_id !== "") {
+        const send = buttonTemplate(elem._id);
+        send.parse_mode = "markdown";
+
+        send.caption = `*Дефект під номером:* ${elem._id}\n*Кімната:* ${
+          elem.room
+        }\n*Опис пошкодження:* ${elem.title}\n*Коментарі:* ${
+          comments ? comments : "відсутні"
+        }`;
         ctx.replyWithPhoto(elem.attachment_id, send);
       } else {
         ctx.replyWithHTML(
-          `Дефект під номером ${elem._id}\n
-           Кімната: <b><i>${elem.room}</i></b>\n
-           Опис пошкодження <b><i>${elem.title}</i></b>`,
-          buttons.fixingBtn(elem._id)
+          `<b><i>Дефект під номером:</i></b> ${elem._id}\n
+           <b><i>Кімната:</i></b> ${elem.room}\n
+           <b><i>Опис пошкодження:</i></b> ${
+             elem.title
+           }\n<b><i>Коментарі:</i></b>${comments ? comments : "відсутні"}`,
+          buttonTemplate(elem._id)
         );
       }
     });
     return details;
   }
 
-  changeDefectStatus(ctx) {
+  changeDefectStatus(ctx, closeReason = "Defect not yet fixed") {
+    this.payload.close_reason = closeReason;
     serviceRequest
       .updateDefectStatus(this.defect_id, this.payload)
       .then((res) => {
@@ -58,6 +53,7 @@ class Render {
           ctx.replyWithHTML(
             `Статус дефекту  успішно змінено на <b>${changeStatus}</b>`
           );
+          ctx.scene.leave();
         }
       })
       .catch((err) => {
@@ -69,9 +65,40 @@ class Render {
             "The user is not in the system. To update the defect information, you must register and gain access."
         ) {
           ctx.reply("У Вас немає доступу для здійснення цієї операції");
+          ctx.scene.leave();
+        } else if (err.response.data.message === "Incorrect defect data") {
+          ctx.reply(
+            "Опис занадто короткий будь ласка введіть не менше 5 символів"
+          );
         } else {
           console.log(err);
         }
+      });
+  }
+  getUser(ctx, comments) {
+    serviceRequest
+      .isAuth(`user/getByUsername/${ctx.from.id.toString()}`)
+      .then((res) => {
+        comments[0].user = res.data.user._id;
+        comments[0].username = res.data.username;
+        this.payload.comments.push(comments[0]);
+        this.payload.status = "fixing";
+        this.addComment(ctx);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  addComment(ctx) {
+    serviceRequest
+      .updateDefectStatus(this.defect_id, this.payload)
+      .then((res) => {
+        if ((res.data.message = "Defect was successfully updated")) {
+          ctx.reply("Коментар успішно додано");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }
   createAction(scene, actionTriger, defects) {
@@ -84,6 +111,8 @@ class Render {
       this.payload.username = ctx.from.id.toString();
       if (this.status === "fixing") {
         this.changeDefectStatus(ctx);
+      } else if (this.status === "comment") {
+        ctx.reply("Введіть ваш коментар");
       } else if (this.status === "solved") {
         ctx.reply("Бажаєте ввести причину закриття", buttons.yesNoKeyboard());
       }
