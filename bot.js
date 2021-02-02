c0119c44a870e201234458d6b72d690e2067fd3d
Serhii Yaroshenko <datextdk@gmail.com>
2021-02-02 14:22:51 +0200

    add webhooks

--- bot.js
+++ bot.js
@@ -2,5 +2,5 @@ const Telegraf = require("telegraf");
 const config = require("./config/keys.config");
 module.exports = bot = new Telegraf(config.token);
 
-// bot.telegram.setWebhook(`${config.url}/bot${config.token}`);
-// bot.startWebhook(`/bot${config.token}`, null, config.port);
+bot.telegram.setWebhook(`${config.url}/bot${config.token}`);
+bot.startWebhook(`/bot${config.token}`, null, config.port);
